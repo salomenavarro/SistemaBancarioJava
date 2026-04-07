7c4b559d7bf1dc6b4e1235b86fcdb65598efde5a
danna_nikoll <dannanikollbarbosa@gmail.com>
2026-04-06 23:53:56 -0500

    Agregar constructores a la clase Credito (completo y basico)

--- src/main/java/com/example/application/modelo/Credito.java
+++ src/main/java/com/example/application/modelo/Credito.java
@@ -18,8 +18,6 @@ public class Credito {
         this.plazoMeses = plazoMeses;
         this.cliente = cliente;
         this.estado = estado;
-
-        asignarTasa();
     }
 
     // CONSTRUCTOR BÁSICO (sobrecarga)
@@ -30,37 +28,5 @@ public class Credito {
         this.plazoMeses = plazoMeses;
         this.cliente = cliente;
         this.estado = "Pendiente";
-
-        asignarTasa();
     }
-
-    // MÉTODO PARA ASIGNAR LA TASA SEGÚN EL TIPO
-    private void asignarTasa() {
-
-        switch (tipo) {
-
-            case "Personal":
-                tasaInteres = 0.012;
-                break;
-
-            case "Hipotecario":
-                tasaInteres = 0.009;
-                break;
-
-            case "Vehicular":
-                tasaInteres = 0.015;
-                break;
-        }
-    }
-
-    // MÉTODO PARA CALCULAR CUOTA MENSUAL
-    public double calcularCuota() {
-
-        double i = tasaInteres;
-        int n = plazoMeses;
-
-        return monto * (i * Math.pow(1 + i, n)) /
-                (Math.pow(1 + i, n) - 1);
-    }
-
 }
