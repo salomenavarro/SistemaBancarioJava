50f1836a4dcdfb3714f313114fc9f00445165c73
emiliano-florez-pelaez <pelaezemiliano8@gmail.com>
2026-04-07 19:02:08 -0500

    Correciones en constructores de Cuentas de Ahorros, Corriente y premium hechas, y tambien se organizo el metodo tranferir con un nuevo condicional para cuentas bloqueadas.

--- src/main/java/com/example/application/modelo/CuentaAhorros.java
+++ src/main/java/com/example/application/modelo/CuentaAhorros.java
@@ -2,13 +2,17 @@ package com.example.application.modelo;
 
 public class CuentaAhorros extends Cuenta {
 
-    public CuentaAhorros(String numeroCuenta, String titular, double saldoInicial, String tipo) {
-        
-        super(numeroCuenta, titular, saldoInicial, tipo);
+    public CuentaAhorros(String numeroCuenta, String titular, double saldoInicial) {
+        //ya no se inicializa el tipo en el constuctor, solo se pone directamente.
+        super(numeroCuenta, titular, saldoInicial, "Ahorros");
     }
 
     @Override
     public boolean retirar(double monto) {
+        // pedimmos que la cuenta no este bloqueda para retirar.
+        if (estaBloqueada())
+            return false; // si esta bloqueada false.
+
         if (monto <= getSaldo()) {
             setSaldo(getSaldo() - monto);
             return true;
--- src/main/java/com/example/application/modelo/CuentaPremium.java
+++ src/main/java/com/example/application/modelo/CuentaPremium.java
@@ -2,14 +2,19 @@ package com.example.application.modelo;
 
 public class CuentaPremium extends Cuenta {
 
-    public CuentaPremium(String numeroCuenta, String titular, double saldoInicial, String tipo) {
-        super(numeroCuenta, titular, saldoInicial, tipo);
+    public CuentaPremium(String numeroCuenta, String titular, double saldoInicial) {
+        //ya no se inicializa el tipo en el constuctor, solo se pone directamente.
+        super(numeroCuenta, titular, saldoInicial,"Premium");
     }
 
     @Override
     public boolean retirar(double monto) {
+        // pedimmos que la cuenta no este bloqueda para retirar.
+        if (estaBloqueada())
+            return false; // si esta bloqueada false.
+
         setSaldo(getSaldo() - monto);
-        return true;
+        return true; // Como esta no tiene restricciones de sobregiro, se ejecuta directo el true.
     }
 
     @Override
